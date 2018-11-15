3344893f794f63c741286aea7d9ec5840ff83779
Lab Mac <runjiexia28@gmail.com>
2018-11-15 09:39:28 +0100

    aggiunta fromStringToInt

--- ViewController.swift
+++ ViewController.swift
@@ -13,7 +13,9 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    func fromStringToInt(stringa : String) -> Int{
+        return Int(stringa)!
+    }
 }
 
