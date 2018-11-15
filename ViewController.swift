e8bc05041a8006a41a64cf8b7149aeefceacb5c2
labmac4 <francesco.bonaldo00@gmail.com>
2018-11-15 09:37:17 +0100

    carico ViewController

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PROJECT5AI_BonaldoXia
+//
+//  Created by Studente on 15/11/2018.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
